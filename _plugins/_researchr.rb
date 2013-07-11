6be37c326f38b16071dac5593427c6740f344641
cicatriz <ryan@geknowm.com>
2013-07-11 15:55:39 +0900

    support highlights generated from papers export

--- _plugins/_researchr.rb
+++ _plugins/_researchr.rb
@@ -1,5 +1,6 @@
 module Jekyll
   require 'bibtex'
+  require 'zlib'
 
   class CiteTag < Liquid::Tag
     def initialize(tag_name, citekey, tokens)
@@ -43,7 +44,64 @@ module Jekyll
         self.data['notes'] = notes_page.content
       end
 
+      hl_file = File.join(base, 'highlights', filename)
+      
+      if File.exists?(hl_file)
+        hl_page = Page.new(site, base, 'highlights', filename)
+        hl_page.render(site.layouts, site.site_payload)
+        self.data['highlights'] = hl_page.content
+      end 
+
       self.data = self.data.merge(ref)
+    end
+  end
+
+  class HighlightsGenerator < Generator
+    safe true
+
+    def generate(site)
+      dir = site.config['highlights_dir'] || 'highlights'
+      hl_file = site.config['highlights_file'] || 'highlights.txt'
+      cur_file = nil
+      
+      File.readlines(hl_file).each do |line|
+        if line =~ /^(.*?) \((\d\d\d\d)\)\. ([^.]*)\.(.*?)(doi:(.*))?$/
+          authors = $1
+          year = $2
+          title = $3
+          doi = $6
+
+          # https://github.com/cparnot/universal-citekey-js/blob/master/universal-citekey.js
+          if doi # gen hash from doi
+            crc = Zlib::crc32(doi)
+            first = 'b'.ord + ((crc % (10*26)) / 26).floor
+            second = 'a'.ord + crc % 26
+            hash = "#{first.chr}#{second.chr}"
+          else # gen hash from title 
+            excluded_characters = "±˙˜´‘’‛“”‟·•!¿¡#∞£¥$%‰&˝¨ˆ¯˘¸˛^~√∫*§◊¬¶†‡≤≥÷:ªº\"\'©®™"
+            #replaced_characters = "°˚+-–—_…,.;ı(){}‹›<>«=≈?|/\\"
+            replaced_characters = "-" # something weird happening with above set
+
+            crc_title = title.downcase.tr(replaced_characters, ' ').tr(excluded_characters, '').gsub(/\s+/,' ')
+            crc = Zlib::crc32(crc_title)
+            first = 't'.ord + ((crc % (4*26)) / 26)
+            second = 'a'.ord + crc % 26
+            hash = "#{first.chr}#{second.chr}"
+          end
+
+          citekey = "#{authors.split(',')[0]}:#{year}#{hash}"
+
+          puts "found highlights for citekey #{citekey}"
+
+          name = "#{citekey}.md"
+          cur_file = File.open(File.join(dir, name), 'w')
+          cur_file.write("")
+        else
+          if cur_file
+            cur_file.puts line
+          end
+        end
+      end
     end
   end
 
